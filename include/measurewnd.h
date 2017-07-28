516af1bd1f06ddbb64d1f945c7d9d3147a44d73c
anmakhort <anmakhort@gmail.com>
2017-07-28 23:02:26 +0200

    prompt to select saving file

--- include/measurewnd.h
+++ include/measurewnd.h
@@ -1,8 +1,15 @@
 #ifndef MEASUREWND_H
 #define MEASUREWND_H
 
+#include <QObject>
 #include <QWidget>
 #include <QCloseEvent>
+#include <QString>
+#include <QFile>
+#include <QTextStream>
+#include <QMessageBox>
+#include <QPushButton>
+#include <QFileDialog>
 
 class MeasureWnd : public QWidget
 {
@@ -17,6 +24,12 @@ signals:
     void s_closing(QObject*);
 
 public slots:
+    void handle_btn_start();
+
+private:
+    QWidget *m_parent;
+    QFile *m_file;
+    QTextStream *m_stream;
 };
 
 #endif // MEASUREWND_H
--- src/measurewnd.cpp
+++ src/measurewnd.cpp
@@ -1,5 +1,4 @@
 #include "../include/measurewnd.h"
-#include <QPushButton>
 
 MeasureWnd::MeasureWnd(QWidget *parent) : QWidget(parent) {
     setAttribute(Qt::WA_DeleteOnClose);
@@ -9,15 +8,35 @@ MeasureWnd::MeasureWnd(QWidget *parent) : QWidget(parent) {
     setFixedSize(300,300);
     setWindowTitle("LCR Measure");
 
-    QPushButton *btn = new QPushButton("TEST", this);
+    QPushButton *btn = new QPushButton("Run", this);
     btn->setGeometry(100,100,100,100);
+
+    connect(btn, SIGNAL(released()), this, SLOT(handle_btn_start()));
 }
 
 MeasureWnd::~MeasureWnd() {
-
+    m_stream->flush();
+    m_file->close();
+    delete m_file;
+    delete m_stream;
 }
 
 void MeasureWnd::closeEvent(QCloseEvent *event) {
     emit s_closing(this);
     event->accept();
+}
+
+void MeasureWnd::handle_btn_start() {
+    if (m_file != NULL) return;
+    QString name = "Select file to save measured data";
+    QString save_path = QFileDialog::getSaveFileName(this, name, tr("/home/an"), tr("DATA (*.dat)"));
+    if (!save_path.endsWith(".dat", Qt::CaseInsensitive)) save_path += ".dat";
+    m_file = new QFile(save_path);
+    if (!m_file->open(QIODevice::WriteOnly | QIODevice::Text)) {
+        QMessageBox(QMessageBox::Information, "Error", m_file->errorString());
+        return;
+    }
+    m_stream = new QTextStream(m_file);
+    *m_stream << "X,Y1,Y2\n";
+    m_stream->flush();
 }
